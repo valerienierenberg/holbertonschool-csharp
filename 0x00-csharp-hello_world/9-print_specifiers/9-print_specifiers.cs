674847516be04b9f7aee2d3d2929444d4670601b
valerienierenberg <valerie.nierenberg@gmail.com>
2021-07-12 20:16:20 +0000

    Add task 9 folder

---
+++ 0x00-csharp-hello_world/9-print_specifiers/9-print_specifiers.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class Program
+{
+	static void Main(string[] args)
+	{
+		double percent = .7553;
+		double currency = 98765.4321;
+        Console.WriteLine(String.Format("Percent: {0:P2}", percent));
+        Console.WriteLine(String.Format("Currency: {0:C}", currency));
+	}
+}
