9b4c81013440f2f259c37ac13e8a65e95ad3beee
valerienierenberg <valerie.nierenberg@gmail.com>
2021-07-13 17:58:20 +0000

    Add task 6 folder

---
+++ 0x01-csharp-ifelse_loops_methods/6-print_comb2/6-print_comb2.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _6_print_comb2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            int b;
+
+            for (a = 0; a <= 9; a++)
+            {
+                for (b = a + 1; b <= 9; b++)
+                {
+                    Console.Write(a);
+                    Console.Write(b);
+                    if (a == 8 && b == 9)
+                    {
+                        Console.WriteLine();
+                    }
+                    else
+                    {
+                        Console.Write(", ");
+                    }
+                }
+            }
+        }
+    }
+}
