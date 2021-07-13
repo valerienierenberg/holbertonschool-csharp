ce14604161b3bbe7a10d8afea1fbcdd3feea699f
valerienierenberg <valerie.nierenberg@gmail.com>
2021-07-13 14:42:06 +0000

    add task 4 folder

---
+++ 0x01-csharp-ifelse_loops_methods/4-print_hexa/4-print_hexa.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _4_print_hexa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i <= 98; i++)
+            {
+                Console.WriteLine(i + " = " + "0x{0:x}", i);
+            }
+        }
+    }
+}
+
