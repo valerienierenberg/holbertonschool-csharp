cb65aba367a9ad1b112f4fe07bfb76ceafc40536
valerienierenberg <valerie.nierenberg@gmail.com>
2021-07-14 17:59:35 +0000

    add task 13 folder

---
+++ 0x02-csharp-arrays_lists_dictionaries/13-best_score/13-best_score.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Dictionary
+{
+    public static string BestScore(Dictionary<string, int> myList)
+    {
+        var max = myList.Aggregate((l, r) => l.Value > r.Value ? l : r).Key;
+        return max;
+    }
+}
