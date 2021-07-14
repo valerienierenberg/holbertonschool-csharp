82dff1d3332ecc86aca580f221bdfe29f2ad620d
valerienierenberg <valerie.nierenberg@gmail.com>
2021-07-14 17:27:45 +0000

    Add task 11 folder

---
+++ 0x02-csharp-arrays_lists_dictionaries/11-multiply_by_2/11-multiply_by_2.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Dictionary
+{
+    public static Dictionary<string, int> MultiplyBy2(Dictionary<string, int> myDict)
+    {
+        var multDict = new Dictionary<string, int>();
+        foreach(var item in myDict)
+        {
+            multDict.Add(item.Key, item.Value * 2);
+        }
+        return multDict;
+    }
+}
