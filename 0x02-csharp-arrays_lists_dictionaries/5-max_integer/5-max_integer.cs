d1cf869ade47fb29f237d8e4b64979df008ba79d
valerienierenberg <valerie.nierenberg@gmail.com>
2021-07-14 15:45:45 +0000

    Add task 5 folder

---
+++ 0x02-csharp-arrays_lists_dictionaries/5-max_integer/5-max_integer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static int MaxInteger(List<int> myList)
+    {
+        if (myList.Count == 0)
+        {
+            Console.WriteLine("List is empty");
+            return (-1);
+        }
+        int max = myList[0];
+        foreach (int temp in myList)
+        {
+            if (temp > max)
+            {
+                max = temp;
+            }
+        }
+        return (max);
+    }
+}
