fdeb28b5f5106a5a2f757efc702feedfbb90ff21
valerienierenberg <valerie.nierenberg@gmail.com>
2021-07-14 19:08:36 +0000

    Add task 100 folder, need to format printing spacing

---
+++ 0x02-csharp-arrays_lists_dictionaries/100-jagged_array/100-jagged_array.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _100_jagged_array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[][] jaggedArray3 =
+            {
+                new int[] { 0, 1, 2, 3 },
+                new int[] { 0, 1, 2, 3, 4, 5, 6 },
+                new int[] { 0, 1 }
+            };
+            for (int i = 0; i < jaggedArray3.GetLength(0); i++)
+            {
+                foreach (int x in jaggedArray3[i])
+                {
+                    Console.Write(x + " ");
+                }
+            }
+        }
+    }
+}
