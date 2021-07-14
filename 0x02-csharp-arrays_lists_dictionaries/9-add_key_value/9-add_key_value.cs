d8b3c10452b845dcc059d41d76f6ad7cb0df5383
valerienierenberg <valerie.nierenberg@gmail.com>
2021-07-14 17:37:17 +0000

    Change notation

--- 0x02-csharp-arrays_lists_dictionaries/9-add_key_value/9-add_key_value.cs
+++ 0x02-csharp-arrays_lists_dictionaries/9-add_key_value/9-add_key_value.cs
@@ -5,7 +5,7 @@ class Dictionary
 {
     public static Dictionary<string, string> AddKeyValue(Dictionary<string, string> myDict, string key, string value)
     {
-        myDict.Add(key, value);
+        myDict[key] = value;
         return myDict;
     }
 }
