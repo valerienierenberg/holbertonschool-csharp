6cc3eceab2c27d72fbd2efc9fa15145f854f4ca3
valerienierenberg <valerie.nierenberg@gmail.com>
2021-07-14 14:33:58 +0000

    Add task 2 folder

---
+++ 0x02-csharp-arrays_lists_dictionaries/2-replace_element/2-replace_element.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Array
+{
+    public static int[] ReplaceElement(int[] array, int index, int n)
+    {
+        if (index < 0 || index >= array.Length)
+        {
+            Console.WriteLine("Index out of range");
+        }
+        else
+        {
+            array[index] = n;
+        }
+        return array;
+    }
+}
