5247ceb19a68f921f4acfb880a2a25d6c58177b3
valerienierenberg <valerie.nierenberg@gmail.com>
2021-07-14 19:24:44 +0000

    add task 100 folder

--- 0x02-csharp-arrays_lists_dictionaries/100-jagged_array/100-jagged_array.cs
+++ 0x02-csharp-arrays_lists_dictionaries/100-jagged_array/100-jagged_array.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace _100_jagged_array
 {
@@ -6,17 +7,22 @@ namespace _100_jagged_array
     {
         static void Main(string[] args)
         {
-            int[][] jaggedArray3 =
+            int[][] jagArray = new int[3][];
+            jagArray[0] = new int[] { 0, 1, 2, 3 };
+            jagArray[1] = new int[] { 0, 1, 2, 3, 4, 5, 6 };
+            jagArray[2] = new int[] { 0, 1 };
+            for (int row = 0; row < jagArray.Length; row++)
             {
-                new int[] { 0, 1, 2, 3 },
-                new int[] { 0, 1, 2, 3, 4, 5, 6 },
-                new int[] { 0, 1 }
-            };
-            for (int i = 0; i < jaggedArray3.GetLength(0); i++)
-            {
-                foreach (int x in jaggedArray3[i])
+                for (int col = 0; col < jagArray[row].Length; col++)
                 {
-                    Console.Write(x + " ");
+                    if (col == jagArray[row].Length - 1)
+                    {
+                        Console.WriteLine(jagArray[row][col]);
+                    }
+                    else
+                    {
+                        Console.Write("{0} ", jagArray[row][col]);
+                    }
                 }
             }
         }
