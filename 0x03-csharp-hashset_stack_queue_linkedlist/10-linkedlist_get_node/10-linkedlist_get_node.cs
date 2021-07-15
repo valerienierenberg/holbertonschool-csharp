806964125a2d6d71ba5c0430d935590b76d9f333
valerienierenberg <valerie.nierenberg@gmail.com>
2021-07-15 21:01:26 +0000

    Add task 10 folder

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/10-linkedlist_get_node/10-linkedlist_get_node.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+    class LList
+    {
+        public static int GetNode(LinkedList<int> myLList, int n)
+        {
+            if (myLList == null || n >= myLList.Count || n < 0)
+            {
+                return (0);
+            }
+            return (myLList.ElementAt(n));
+        }
+    }
