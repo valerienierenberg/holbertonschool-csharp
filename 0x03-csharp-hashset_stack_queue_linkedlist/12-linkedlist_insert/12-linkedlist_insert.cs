5c0ae425d1b8c0f1dc4e1d8254bc72bc3cccbbd1
valerienierenberg <valerie.nierenberg@gmail.com>
2021-07-15 21:38:33 +0000

    Add task 12 folder

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/12-linkedlist_insert/12-linkedlist_insert.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static LinkedListNode<int> Insert(LinkedList<int> myLList, int n)
+    {
+        LinkedListNode<int> newNode = new LinkedListNode<int>(n);
+        // initialize node to head of linked list
+        LinkedListNode<int> current = myLList.First;
+        while (current != null)
+        {
+            if (current.Value > n)
+            {
+                myLList.AddBefore(current, newNode);
+                break;
+            }
+            // if we made it to the end of the linked list and there wasn't a number higher than n, put the node at the end of the list
+            if (current.Next == null)
+            {
+                myLList.AddAfter(current, newNode);
+                break;
+            }
+            current = current.Next;
+        }
+        return (newNode);
+    }
+}
