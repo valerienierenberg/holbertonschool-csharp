848c878b797e16f6947c0603494e597c546f0969
valerienierenberg <valerie.nierenberg@gmail.com>
2021-07-15 18:47:46 +0000

    Add task 8 folder

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/8-linkedlist_find/8-linkedlist_find.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class LList
+{
+    public static int FindNode(LinkedList<int> myLList, int value)
+    {
+        if (!myLList.Contains(value))
+        {
+            return (-1);
+        }
+        else
+        {
+            int i = myLList.TakeWhile(n => n != value).Count();
+            return i;
+        }
+    }
+}
