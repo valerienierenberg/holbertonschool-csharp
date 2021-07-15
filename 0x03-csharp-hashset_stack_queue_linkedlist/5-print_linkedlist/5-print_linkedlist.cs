ae85f6e944a7e63deed9d84613799aa4b0960a35
valerienierenberg <valerie.nierenberg@gmail.com>
2021-07-15 17:00:23 +0000

    Add task 5 folder

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/5-print_linkedlist/5-print_linkedlist.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static LinkedList<int> CreatePrint(int size)
+    {
+        LinkedList<int> newList = new LinkedList<int>();
+        for (int i = size - 1; i >= 0; i--)
+        {
+            newList.AddFirst(i);
+        }
+        foreach (int value in newList)
+        {
+            Console.WriteLine(value.ToString());
+        }
+        return newList;
+    }
+}
