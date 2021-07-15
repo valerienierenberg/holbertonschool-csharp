f7031ecad60f53bdbb6236885d241f252db15267
valerienierenberg <valerie.nierenberg@gmail.com>
2021-07-15 16:25:31 +0000

    Add task 4 folder

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/4-queue_enqueue_dequeue/4-queue_enqueue_dequeue.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class MyQueue
+{
+    public static Queue<string> Info(Queue<string> aQueue, string newItem, string search)
+    {
+        Console.WriteLine("Number of items: " + aQueue.Count);
+        Console.WriteLine("First item: {0}", aQueue.Peek());
+        if (aQueue.Count == 0)
+        {
+            Console.WriteLine("Queue is empty");
+        }
+        Console.WriteLine("Queue contains \"{0}\": {1}", search, aQueue.Contains(search));
+        if (aQueue.Contains(search))
+        {
+            int indexOfSearch = aQueue.ToArray().ToList().IndexOf(search);
+            while (aQueue.Count > indexOfSearch + 1)
+            {
+                aQueue.Dequeue();
+            }
+        }
+        aQueue.Enqueue(newItem);
+        return aQueue;
+    }
+}
