c4cd4f64416a85f2b3e6b4e084071976bef0b8c5
valerienierenberg <valerie.nierenberg@gmail.com>
2021-07-26 08:17:46 -0500

    Add task 0

---
+++ 0x04-csharp-exceptions/0-safe_list_print/0-safe_list_print.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+    class List
+    {
+        public static int SafePrint(List<int> myList, int n)
+        {
+            int count = 0;
+            try
+            {
+                for (int i = 0; i <= myList.Count - 1; i++)
+                {
+                    if (i <= n - 1)
+                    {
+                        Console.WriteLine(myList[i]);
+                        count += 1;
+                    }
+                }
+            }
+            catch (ArgumentOutOfRangeException)
+            {
+                Console.WriteLine("Catch");
+            }
+            return count;
+        }
+    }
