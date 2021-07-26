a9c0b197899b24a5fd42aa828f0de355eeb2f6b5
valerienierenberg <valerie.nierenberg@gmail.com>
2021-07-26 08:32:49 -0500

    Add task 1

---
+++ 0x04-csharp-exceptions/1-divide_print/1-divide_print.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+    class Int
+    {
+        public static void divide(int a, int b)
+        {
+            try
+            {
+                int res = a / b;
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("Cannot divide by zero");
+            }
+            finally
+            {
+                if (b == 0)
+                {
+                    Console.WriteLine("{0} / {1} = {2}", a, b, 0);
+                }
+                else
+                {
+                    Console.WriteLine("{0} / {1} = {2}", a, b, a / b);
+                }
+            }
+        }
+    }
