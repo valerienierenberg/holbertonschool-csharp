4ecc90e63ecb8312ce1d40b351409bcd39be2f83
valerienierenberg <valerie.nierenberg@gmail.com>
2021-07-27 09:41:46 -0500

    Add task 4

---
+++ 0x06-csharp-classes/4-enemy/4-enemy.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Enemies
+{
+    ///<summary>Represents Zombie class</summary>
+    class Zombie
+    {
+        public int health;
+
+        public Zombie()
+        {
+            health = 0;
+        }
+        public Zombie(int value)
+        {
+            health = value;
+            if (value < 0)
+            {
+                throw new ArgumentException("Health must be greater than or equal to 0");
+            }
+        }
+        private string _name = "(No name)";
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                _name = value;
+            }
+        }
+        public int GetHealth()
+        {
+            return health;
+        }
+    }
+}
