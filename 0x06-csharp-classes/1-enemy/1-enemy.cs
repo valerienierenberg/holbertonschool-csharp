5315ef461800b3c408687457b9d9796334cbe0f7
valerienierenberg <valerie.nierenberg@gmail.com>
2021-07-27 09:15:49 -0500

    Add task 1

---
+++ 0x06-csharp-classes/1-enemy/1-enemy.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Enemies
+{
+    ///<summary>Represents Zombie class</summary>
+    class Zombie
+    {
+        public int health;
+
+        public Zombie()
+        {
+            health = 0;
+        }
+    }
+}
