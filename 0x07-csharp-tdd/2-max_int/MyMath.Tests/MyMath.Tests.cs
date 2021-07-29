fa2e77e6ec7c1a949bcd3480a4a05f7a87dee6eb
valerienierenberg <valerie.nierenberg@gmail.com>
2021-07-29 14:52:23 +0000

    Add tests for max being in middle or beginning of list, empty list, or if the list has a negative number

--- 0x07-csharp-tdd/2-max_int/MyMath.Tests/MyMath.Tests.cs
+++ 0x07-csharp-tdd/2-max_int/MyMath.Tests/MyMath.Tests.cs
@@ -8,7 +8,7 @@ namespace MyMath.Tests
     public class Tests
     {
         [Test]
-        public void Test1()
+        public void TestLast()
         {
             List<int> myList = new List<int>(); 
 
@@ -19,6 +19,58 @@ namespace MyMath.Tests
             int output = Operations.Max(myList);
 
             Assert.AreEqual(3, output);
+        }
+
+        [Test]
+        public void TestFirst()
+        {
+            List<int> myList = new List<int>(); 
+
+            myList.Add(3);
+            myList.Add(2);
+            myList.Add(1);
+            
+            int output = Operations.Max(myList);
+
+            Assert.AreEqual(3, output);
+        }
+
+        [Test]
+        public void TestMiddle()
+        {
+            List<int> myList = new List<int>(); 
+
+            myList.Add(3);
+            myList.Add(4);
+            myList.Add(1);
+            
+            int output = Operations.Max(myList);
+
+            Assert.AreEqual(4, output);
+        }
+
+        [Test]
+        public void TestNegInList()
+        {
+            List<int> myList = new List<int>(); 
+
+            myList.Add(3);
+            myList.Add(-3);
+            myList.Add(1);
+            
+            int output = Operations.Max(myList);
+
+            Assert.AreEqual(3, output);
+        }
+
+        [Test]
+        public void TestEmptyList()
+        {
+            List<int> myList = new List<int>(); 
+            
+            int output = Operations.Max(myList);
+
+            Assert.AreEqual(0, output);
         }
     }
 }
