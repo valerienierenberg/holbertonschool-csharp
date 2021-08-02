bbb6ba1be864cd663627b464c40d27e4e79d4493
valerienierenberg <valerie.nierenberg@gmail.com>
2021-08-02 08:57:24 -0500

    Add task 2

---
+++ 0x08-csharp-inheritance/2-subclass/2-subclass.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+///<summary>Class Obj</summary>
+class Obj
+{
+    ///<summary>IsOnlyASubclass method</summary>
+    public static bool IsOnlyASubclass(Type derivedType, Type baseType)
+    {
+        return (derivedType.IsSubclassOf(baseType));
+    }
+}
