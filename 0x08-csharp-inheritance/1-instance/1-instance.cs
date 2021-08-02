6b3125161e71adb164817c7d3fc24b7c1c67dd7d
valerienierenberg <valerie.nierenberg@gmail.com>
2021-08-02 08:51:46 -0500

    Change method name

--- 0x08-csharp-inheritance/1-instance/1-instance.cs
+++ 0x08-csharp-inheritance/1-instance/1-instance.cs
@@ -3,7 +3,7 @@
 ///<summary>Class Obj</summary>
 class Obj
 {
-    ///<summary>IsOfTypeInt method</summary>
+    ///<summary>IsjInstanceOfArray method</summary>
     public static bool IsInstanceOfArray(object obj)
     {
         return (typeof(Array).IsInstanceOfType(obj));
