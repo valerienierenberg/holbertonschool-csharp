f0810e740fd6150fdbb5fe9ad183c4f25707363e
valerienierenberg <valerie.nierenberg@gmail.com>
2021-08-02 08:49:11 -0500

    Add documentation

--- 0x08-csharp-inheritance/1-instance/1-instance.cs
+++ 0x08-csharp-inheritance/1-instance/1-instance.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 
+///<summary>Class Obj</summary>
 class Obj
 {
+    ///<summary>IsOfTypeInt method</summary>
     public static bool IsInstanceOfArray(object obj)
     {
         return (typeof(Array).IsInstanceOfType(obj));
