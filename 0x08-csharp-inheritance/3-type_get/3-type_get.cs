0b266fd9b2fb2ec398f4c3f5fcdc249bda888d06
valerienierenberg <valerie.nierenberg@gmail.com>
2021-08-02 10:14:31 -0500

    Add task 3

---
+++ 0x08-csharp-inheritance/3-type_get/3-type_get.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Reflection;
+using System.Linq;
+
+///<summary>Class Obj</summary>
+class Obj
+{
+    ///<summary>IsOnlyASubclass method</summary>
+    public static void Print(object myObj)
+    {
+        PropertyInfo[] pInfo;
+        MethodInfo[] mInfo;
+
+        pInfo = myObj.GetType().GetProperties();
+        mInfo = myObj.GetType().GetMethods();
+        Console.WriteLine("{0} Properties:", myObj.GetType().Name); // prints the type of the object
+        for (int i = 0; i < pInfo.Length; i++)
+        {
+            Console.WriteLine(pInfo[i].Name);
+        }
+        Console.WriteLine("{0} Methods:", myObj.GetType().Name); // prints the type of the object
+        for (int i = 0; i < mInfo.Length; i++)
+        {
+            Console.WriteLine(mInfo[i].Name);
+        }
+    }
+}
