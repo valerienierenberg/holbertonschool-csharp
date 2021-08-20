0259b904a29f0d77c47241d34cb3192002e6a4fc
valerienierenberg <valerie.nierenberg@gmail.com>
2021-08-20 13:56:44 -0500

    Add task 24

---
+++ 0x09-csharp-linear_algebra/24-matrix_shear_2D/24-main.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Windows;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        double[,] matrixOne2d =  { { 1, 2 }, { 3, 4 } };
+        char direction = 'x';
+        double factor = 2;
+        double[,] res2d;
+
+        res2d = MatrixMath.Shear2D(matrixOne2d, direction, factor);
+
+        for (int i = 0; i < res2d.GetLength(0); i++)
+        {
+            for (int j = 0; j < res2d.GetLength(1); j++)
+            {
+                Console.Write(res2d[i, j]);
+                if (j != res2d.GetLength(1) - 1)
+                    Console.Write(", ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ 0x09-csharp-linear_algebra/24-matrix_shear_2D/24_matrix_shear_2D.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+class MatrixMath
+{
+    public static double[,] Shear2D(double[,] matrix, char direction, double factor)
+    {
+        double[,] newMatrixDefault = new double[,] {{-1}};
+        double[,] newMatrix = new double[2, 2];
+        double[,] shear = new double[2, 2] {{1, 0}, {0, 1}};
+
+        if (direction != 'x' && direction != 'y')
+        {
+            return newMatrixDefault;
+        }
+
+        if (direction == 'x')
+        {
+            shear[1, 0] = factor;
+        }
+
+        if (direction == 'y')
+        {
+            shear[0, 1] = factor;
+        }
+
+        if (matrix.GetLength(0) != 2 && matrix.GetLength(1) != 2)
+        {
+            return newMatrixDefault;
+        }
+
+        for (int i = 0; i < 2; i++)
+        {
+            for (int j = 0; j < 2; j++)
+            {
+              for (int k = 0; j < 2; j++)  
+                {
+                    newMatrix[i, j] = newMatrix[i, j] + (matrix[i, k] * shear[k, j]);
+                }
+            }
+        }
+        return newMatrix;
+    }
+}
