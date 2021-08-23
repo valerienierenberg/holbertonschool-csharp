dc71d2bd64de6718c468cc47c2d79a1b76817a66
valerienierenberg <valerie.nierenberg@gmail.com>
2021-08-23 14:02:56 -0500

    fix brackets

--- 0x09-csharp-linear_algebra/33-inverse_2D/33-inverse_2D.cs
+++ 0x09-csharp-linear_algebra/33-inverse_2D/33-inverse_2D.cs
@@ -3,32 +3,33 @@
 class MatrixMath
 {
     public static double[,] Inverse2D(double[,] matrix)
-{
-    // assumes determinant is not 0
-    // that is, the matrix does have an inverse
-    int n = matrix.Length;
-    double[][] result = MatrixCreate(n, n); // make a copy of matrix
-    for (int i = 0; i < n; ++i)
-    for (int j = 0; j < n; ++j)
-        result[i][j] = matrix[i][j];
-
-    double[][] lum; // combined lower & upper
-    int[] perm;
-    int toggle;
-    toggle = MatrixDecompose(matrix, out lum, out perm);
-
-    double[] b = new double[n];
-    for (int i = 0; i < n; ++i)
     {
-    for (int j = 0; j < n; ++j)
-        if (i == perm[j])
-        b[j] = 1.0;
-        else
-        b[j] = 0.0;
+        // assumes determinant is not 0
+        // that is, the matrix does have an inverse
+        int n = matrix.Length;
+        double[][] result = MatrixCreate(n, n); // make a copy of matrix
+        for (int i = 0; i < n; ++i)
+        for (int j = 0; j < n; ++j)
+            result[i][j] = matrix[i][j];
 
-    double[] x = Helper(lum, b); // 
-    for (int j = 0; j < n; ++j)
-        result[j][i] = x[j];
+        double[][] lum; // combined lower & upper
+        int[] perm;
+        int toggle;
+        toggle = MatrixDecompose(matrix, out lum, out perm);
+
+        double[] b = new double[n];
+        for (int i = 0; i < n; ++i)
+        {
+        for (int j = 0; j < n; ++j)
+            if (i == perm[j])
+            b[j] = 1.0;
+            else
+            b[j] = 0.0;
+
+        double[] x = Helper(lum, b); // 
+        for (int j = 0; j < n; ++j)
+            result[j][i] = x[j];
+        }
+        return result;
     }
-    return result;
 }
