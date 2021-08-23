dbb86fac52fefe399f01e14af17720ee55efae3e
valerienierenberg <valerie.nierenberg@gmail.com>
2021-08-23 09:42:31 -0500

    change to parentheses

---
+++ 0x09-csharp-linear_algebra/30-cross_product/30_cross_product.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _30_cross_product
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ 0x09-csharp-linear_algebra/33-inverse_2D/33-inverse_2D.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _33_inverse_2D
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
