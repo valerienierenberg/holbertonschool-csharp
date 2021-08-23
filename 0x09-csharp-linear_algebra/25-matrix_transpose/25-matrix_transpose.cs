0cef104aec9bee037fe9fe6c65afe26635b340f3
valerienierenberg <valerie.nierenberg@gmail.com>
2021-08-23 12:56:16 -0500

    Switch dimensions

--- 0x09-csharp-linear_algebra/25-matrix_transpose/25-matrix_transpose.cs
+++ 0x09-csharp-linear_algebra/25-matrix_transpose/25-matrix_transpose.cs
@@ -5,7 +5,7 @@ class MatrixMath
     public static double[,] Transpose(double[,] matrix)
     {
         double[,] newMatrixDefault = new double[,] {{}};
-        double[,] newMatrix = new double[matrix.GetLength(0), matrix.GetLength(1)];
+        double[,] newMatrix = new double[matrix.GetLength(1), matrix.GetLength(0)];
 
         for (int i = 0; i < matrix.GetLength(0); i++)
         {
