68048442c20b9f965d0ace88524daa6af6eba14e
valerienierenberg <valerie.nierenberg@gmail.com>
2021-08-23 14:04:48 -0500

    simplify

--- 0x09-csharp-linear_algebra/33-inverse_2D/33-inverse_2D.cs
+++ 0x09-csharp-linear_algebra/33-inverse_2D/33-inverse_2D.cs
@@ -4,32 +4,18 @@ class MatrixMath
 {
     public static double[,] Inverse2D(double[,] matrix)
     {
-        // assumes determinant is not 0
-        // that is, the matrix does have an inverse
-        int n = matrix.Length;
-        double[][] result = MatrixCreate(n, n); // make a copy of matrix
-        for (int i = 0; i < n; ++i)
-        for (int j = 0; j < n; ++j)
-            result[i][j] = matrix[i][j];
+        var rowCount = matrix.GetLength(0);  
+        var colCount = matrix.GetLength(1);  
 
-        double[][] lum; // combined lower & upper
-        int[] perm;
-        int toggle;
-        toggle = MatrixDecompose(matrix, out lum, out perm);
+        if (rowCount != colCount)  
+            throw new InvalidOperationException(Properties.Resources.Exception_RequiredSquareMatrix);  
 
-        double[] b = new double[n];
-        for (int i = 0; i < n; ++i)
-        {
-        for (int j = 0; j < n; ++j)
-            if (i == perm[j])
-            b[j] = 1.0;
-            else
-            b[j] = 0.0;
+        var newMatrix = ConcatHorizontally(matrix, CreateIdentityMatrix(rowCount));  
 
-        double[] x = Helper(lum, b); // 
-        for (int j = 0; j < n; ++j)
-            result[j][i] = x[j];
-        }
-        return result;
-    }
+        var result = Eliminate(newMatrix, MatrixReductionForm.ReducedRowEchelonForm, rowCount);  
+        if (result.Rank != colCount)  
+            return null;  
+
+        return result.Solution;  
+    }   
 }
