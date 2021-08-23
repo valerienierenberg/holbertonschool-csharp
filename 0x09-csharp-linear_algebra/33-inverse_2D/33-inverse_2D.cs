0a1be55a0e19e3e16c93f0cb7db85dd58097fb57
valerienierenberg <valerie.nierenberg@gmail.com>
2021-08-23 13:59:01 -0500

    Attempt at matrix inversion

--- 0x09-csharp-linear_algebra/33-inverse_2D/33-inverse_2D.cs
+++ 0x09-csharp-linear_algebra/33-inverse_2D/33-inverse_2D.cs
@@ -1,12 +1,65 @@
 ﻿using System;
 
-namespace _33_inverse_2D
+class MatrixMath
 {
-    class Program
+    public static double[,] Inverse2D(double[,] matrix)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+        int n = matrix.Length;
+        double[,] newMatrixDefault = new double[,] {{-1}};
+        double[,] result = new double[2, 2];
+
+        int[] perm;
+        int toggle;
+//        double[][] lum = MatrixDecompose(matrix, out perm,
+//            out toggle);
+//        if (lum == null)
+//            throw new Exception("Unable to compute inverse");
+
+        double[] b = new double[n];
+        for (int i = 0; i < n; ++i)
+    {
+            for (int j = 0; j < n; ++j)
+    {
+                if (i == perm[j])
+                    b[j] = 1.0;
+                else
+                    b[j] = 0.0;
+            }
+
+            double[] x = HelperSolve(lum, b);
+
+            for (int j = 0; j < n; ++j)
+        result[j][i] = x[j];
         }
+        return result;
+    }
+
+
+    static double[] HelperSolve(double[][] luMatrix, double[] b)
+    {
+        // before calling this helper, permute b using the perm array
+        // from MatrixDecompose that generated luMatrix
+        int n = luMatrix.Length;
+        double[] x = new double[n];
+        b.CopyTo(x, 0);
+
+        for (int i = 1; i < n; ++i)
+        {
+            double sum = x[i];
+            for (int j = 0; j < i; ++j)
+                sum -= luMatrix[i][j] * x[j];
+            x[i] = sum;
+        }
+
+        x[n - 1] /= luMatrix[n - 1][n - 1];
+        for (int i = n - 2; i >= 0; --i)
+        {
+            double sum = x[i];
+            for (int j = i + 1; j < n; ++j)
+                sum -= luMatrix[i][j] * x[j];
+            x[i] = sum / luMatrix[i][i];
+        }
+
+        return x;
     }
 }
