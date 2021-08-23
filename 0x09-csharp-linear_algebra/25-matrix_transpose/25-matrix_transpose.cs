2e4bc0c18aca1a1ecafe3400738f46dc626217ef
valerienierenberg <valerie.nierenberg@gmail.com>
2021-08-23 12:54:54 -0500

    Modify to work for all sizes

--- 0x09-csharp-linear_algebra/25-matrix_transpose/25-matrix_transpose.cs
+++ 0x09-csharp-linear_algebra/25-matrix_transpose/25-matrix_transpose.cs
@@ -4,12 +4,12 @@ class MatrixMath
 {
     public static double[,] Transpose(double[,] matrix)
     {
-        double[,] newMatrixDefault = new double[,] {{-1}};
-        double[,] newMatrix = new double[2, 2];
+        double[,] newMatrixDefault = new double[,] {{}};
+        double[,] newMatrix = new double[matrix.GetLength(0), matrix.GetLength(1)];
 
-        for (int i = 0; i < 2; i++)
+        for (int i = 0; i < matrix.GetLength(0); i++)
         {
-            for (int j = 0; j < 2; j++)
+            for (int j = 0; j < matrix.GetLength(1); j++)
             {
                 newMatrix[i, j] = matrix[j, i];
             }
