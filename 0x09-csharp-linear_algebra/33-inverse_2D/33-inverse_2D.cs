c7a04dec8700aba3722ab74bb755e939cf55b195
valerienierenberg <valerie.nierenberg@gmail.com>
2021-08-23 14:01:36 -0500

    Another attempt

--- 0x09-csharp-linear_algebra/33-inverse_2D/33-inverse_2D.cs
+++ 0x09-csharp-linear_algebra/33-inverse_2D/33-inverse_2D.cs
@@ -3,63 +3,32 @@
 class MatrixMath
 {
     public static double[,] Inverse2D(double[,] matrix)
+{
+    // assumes determinant is not 0
+    // that is, the matrix does have an inverse
+    int n = matrix.Length;
+    double[][] result = MatrixCreate(n, n); // make a copy of matrix
+    for (int i = 0; i < n; ++i)
+    for (int j = 0; j < n; ++j)
+        result[i][j] = matrix[i][j];
+
+    double[][] lum; // combined lower & upper
+    int[] perm;
+    int toggle;
+    toggle = MatrixDecompose(matrix, out lum, out perm);
+
+    double[] b = new double[n];
+    for (int i = 0; i < n; ++i)
     {
-        int n = matrix.Length;
-        double[,] newMatrixDefault = new double[,] {{-1}};
-        double[,] result = new double[2, 2];
+    for (int j = 0; j < n; ++j)
+        if (i == perm[j])
+        b[j] = 1.0;
+        else
+        b[j] = 0.0;
 
-        int[] perm;
-        int toggle;
-//        double[][] lum = MatrixDecompose(matrix, out perm,
-//            out toggle);
-//        if (lum == null)
-//            throw new Exception("Unable to compute inverse");
-
-        double[] b = new double[n];
-        for (int i = 0; i < n; ++i)
-    {
-            for (int j = 0; j < n; ++j)
-    {
-                if (i == perm[j])
-                    b[j] = 1.0;
-                else
-                    b[j] = 0.0;
-            }
-
-            double[] x = HelperSolve(lum, b);
-
-            for (int j = 0; j < n; ++j)
+    double[] x = Helper(lum, b); // 
+    for (int j = 0; j < n; ++j)
         result[j][i] = x[j];
-        }
-        return result;
     }
-
-
-    static double[] HelperSolve(double[][] luMatrix, double[] b)
-    {
-        // before calling this helper, permute b using the perm array
-        // from MatrixDecompose that generated luMatrix
-        int n = luMatrix.Length;
-        double[] x = new double[n];
-        b.CopyTo(x, 0);
-
-        for (int i = 1; i < n; ++i)
-        {
-            double sum = x[i];
-            for (int j = 0; j < i; ++j)
-                sum -= luMatrix[i][j] * x[j];
-            x[i] = sum;
-        }
-
-        x[n - 1] /= luMatrix[n - 1][n - 1];
-        for (int i = n - 2; i >= 0; --i)
-        {
-            double sum = x[i];
-            for (int j = i + 1; j < n; ++j)
-                sum -= luMatrix[i][j] * x[j];
-            x[i] = sum / luMatrix[i][i];
-        }
-
-        return x;
-    }
+    return result;
 }
