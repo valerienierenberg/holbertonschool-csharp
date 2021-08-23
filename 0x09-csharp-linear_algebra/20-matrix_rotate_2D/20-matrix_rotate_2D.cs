a1bf99566073009103639e965900f804fd4d8882
valerienierenberg <valerie.nierenberg@gmail.com>
2021-08-23 10:20:34 -0500

    Use getlength variables

--- 0x09-csharp-linear_algebra/20-matrix_rotate_2D/20-matrix_rotate_2D.cs
+++ 0x09-csharp-linear_algebra/20-matrix_rotate_2D/20-matrix_rotate_2D.cs
@@ -6,22 +6,23 @@ class MatrixMath
     ///<summary>Method that rotates a square 2D matrix by a given angle in radians and returns the resulting matrix</summary>
     public static double[,] Rotate2D(double[,] matrix, double angle)
     {
-        // resource - https://math.stackexchange.com/questions/1404484/how-is-that-a-rotation-by-an-angle-%CE%B8-about-the-origin-can-be-represented-by-this
-        double[,] newMatrixDefault = new double[,] {{-1}};
+        int row = matrix.GetLength(0);
+        int column = matrix.GetLength(1);
         double[,] newMatrix = new double[2, 2];
         double[,] rotate = new double[2, 2] {{Math.Cos(angle), Math.Sin(angle)}, {-1 * Math.Sin(angle), Math.Cos(angle)}};
-        
-        if (matrix.GetLength(0) != 2 || matrix.GetLength(1) != 2)
+        double[,] default = new double[,] {{-1}};
+
+        if (row != 2 || column != 2)
         {
-            return newMatrixDefault;
+           return default;
         }
-        for (int i = 0; i < 2; i++)
+        for (int i = 0; i < row; i++)
         {
-            for (int j = 0; j < 2; j++)
+            for (int j = 0; j < column; j++)
             {
-              for (int k = 0; j < 2; j++)  
+                for (int k = 0; k < 2; k++)
                 {
-                    newMatrix[i, j] = Math.Round(newMatrix[i, j] + (matrix[i, k] * rotate[k, j]), 2);
+                    matrix3[i, j] = Math.Round(matrix3[i, j] + (matrix[i, k] * rotate[k, j]), 2);
                 }
             }
         }
