9652598eb4403715e5cdd3eedef2a8bd6bf72d80
valerienierenberg <valerie.nierenberg@gmail.com>
2021-08-24 10:40:09 -0500

    Add task 33

--- 0x09-csharp-linear_algebra/33-inverse_2D/33-inverse_2D.cs
+++ 0x09-csharp-linear_algebra/33-inverse_2D/33-inverse_2D.cs
@@ -2,20 +2,24 @@
 
 class MatrixMath
 {
+    ///<summary>method that returns inverse</summary>
     public static double[,] Inverse2D(double[,] matrix)
     {
-        var rowCount = matrix.GetLength(0);  
-        var colCount = matrix.GetLength(1);  
+        int row = matrix.GetLength(0);
+        int column = matrix.GetLength(1);
+        
+        if (row != 2 && column != 2)
+        {
+            return new double[,] {{-1}};
+        }
 
-        if (rowCount != colCount)  
-            return -1;
+        double det = (matrix[0,0] * matrix[1, 1]) - (matrix[0, 1] * matrix[1, 0]);
+        if (det == 0)
+        {
+            return new double[,] {{-1}};
+        }
 
-        var newMatrix = ConcatHorizontally(matrix, CreateIdentityMatrix(rowCount));  
-
-        var result = Eliminate(newMatrix, MatrixReductionForm.ReducedRowEchelonForm, rowCount);  
-        if (result.Rank != colCount)  
-            return null;  
-
-        return result.Solution;  
-    }   
+        double[,] inverse = new double[,] {{Math.Round((1 / det) * matrix[1, 1], 2), Math.Round((1 / det) * -matrix[0, 1], 2)}, {Math.Round((1 / det) * -matrix[1, 0], 2), Math.Round((1 / det) * matrix[0, 0],2)}};
+        return inverse;
+    }
 }
