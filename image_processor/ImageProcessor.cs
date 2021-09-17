ab37d022810612b1743c2beaaedc9103c288f841
valerienierenberg <valerie.nierenberg@gmail.com>
2021-09-17 12:37:42 -0500

    Check 0 and 1

--- image_processor/ImageProcessor.cs
+++ image_processor/ImageProcessor.cs
@@ -14,9 +14,9 @@ class ImageProcessor
             {
                 for (int y = 0; y < bitmap.Height; y++)
                 {
-                    Color color = bitmap.GetPixel(x, y);
-                    color = Color.FromArgb(255, (255 - color.R), (255 - color.G), (255 - color.B));
-                    bitmap.SetPixel(x, y, color);
+                    c = bitmap.GetPixel(x, y);
+                    c = Color.FromArgb(255, (255 - c.R), (255 - c.G), (255 - c.B));
+                    bitmap.SetPixel(x, y, c);
                 }
             }
             string new_filename = Path.GetFileNameWithoutExtension(filename) + "_inverse" + Path.GetExtension(filename);
@@ -27,10 +27,9 @@ class ImageProcessor
     /// <summary> Inverse method </summary>
     public static void Grayscale(string[] filenames) 
     {
-        foreach (string file in filenames)
+        foreach (string filename in filenames)
         {
-            Bitmap temp = new Bitmap(file);
-            Bitmap bmap = (Bitmap)temp.Clone();
+            Bitmap bitmap = new Bitmap(file);
             Color c;
 
             for (int i = 0; i < bmap.Width; i++)
@@ -51,35 +50,30 @@ class ImageProcessor
     public static void BlackWhite(string[] filenames, double threshold) {
     foreach (string file in filenames)
     {
-    string fileWithoutImages = file.Remove(0, 7);
-    string fileWithoutImagesAndJPG = fileWithoutImages.Remove(fileWithoutImages.Length - 4, 4);
-    string fileBW = fileWithoutImagesAndJPG + "_bw" + ".jpg";
-    // Console.WriteLine(fileBW);
+        Bitmap temp = new Bitmap(file);
+        Bitmap bmap = (Bitmap)temp.Clone();
+        Color c;
 
-    Bitmap temp = new Bitmap(file);
-    Bitmap bmap = (Bitmap)temp.Clone();
-    Color c;
-
-    for (int i = 0; i < bmap.Width; i++)
-    {
-    for (int j = 0; j < bmap.Height; j++)
-    {
-    c = bmap.GetPixel(i, j);
-    double luminance = ((c.R * 0.3) + (c.G * 0.59) + (c.B * 0.11));
-    if (luminance < threshold)
-    {
-    bmap.SetPixel(i, j, Color.FromArgb(0, 0, 0));
-    }
-    else
-    {
-    bmap.SetPixel(i, j, Color.FromArgb(255, 255, 255));
-    }
-    //byte gray = (byte)(.299 * c.R + .587 * c.G + .114 * c.B);
-    //bmap.SetPixel(i, j, Color.FromArgb(gray, gray, gray));
-    }
-    }
-    temp = (Bitmap)bmap.Clone();
-    temp.Save("C:\\Users\\Valerie\\holbertonschool-csharp\\image_processor\\" + fileBW);
+        for (int i = 0; i < bmap.Width; i++)
+        {
+        for (int j = 0; j < bmap.Height; j++)
+        {
+        c = bmap.GetPixel(i, j);
+        double luminance = ((c.R * 0.3) + (c.G * 0.59) + (c.B * 0.11));
+        if (luminance < threshold)
+        {
+        bmap.SetPixel(i, j, Color.FromArgb(0, 0, 0));
+        }
+        else
+        {
+        bmap.SetPixel(i, j, Color.FromArgb(255, 255, 255));
+        }
+        //byte gray = (byte)(.299 * c.R + .587 * c.G + .114 * c.B);
+        //bmap.SetPixel(i, j, Color.FromArgb(gray, gray, gray));
+        }
+        }
+        temp = (Bitmap)bmap.Clone();
+        temp.Save("C:\\Users\\Valerie\\holbertonschool-csharp\\image_processor\\" + fileBW);
     }
     }
 
