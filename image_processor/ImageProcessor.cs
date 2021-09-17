63729041dba278281e34181a069a3737744220f3
valerienierenberg <valerie.nierenberg@gmail.com>
2021-09-17 12:36:02 -0500

    Change grayscale to work with filenames

--- image_processor/ImageProcessor.cs
+++ image_processor/ImageProcessor.cs
@@ -24,69 +24,64 @@ class ImageProcessor
         }
     }
 
- /// <summary> Inverse method </summary>
- public static void Grayscale(string[] filenames) 
- {
- foreach (string file in filenames)
- {
- string fileWithoutImages = file.Remove(0, 7);
- string fileWithoutImagesAndJPG = fileWithoutImages.Remove(fileWithoutImages.Length - 4, 4);
- string fileGrayscale = fileWithoutImagesAndJPG + "_grayscale" + ".jpg";
- // Console.WriteLine(fileGrayscale);
+    /// <summary> Inverse method </summary>
+    public static void Grayscale(string[] filenames) 
+    {
+        foreach (string file in filenames)
+        {
+            Bitmap temp = new Bitmap(file);
+            Bitmap bmap = (Bitmap)temp.Clone();
+            Color c;
 
- Bitmap temp = new Bitmap(file);
- Bitmap bmap = (Bitmap)temp.Clone();
- Color c;
+            for (int i = 0; i < bmap.Width; i++)
+            {
+                for (int j = 0; j < bmap.Height; j++)
+                {
+                    c = bmap.GetPixel(i, j);
+                    byte gray = (byte)(.299 * c.R + .587 * c.G + .114 * c.B);
+                    bmap.SetPixel(i, j, Color.FromArgb(gray, gray, gray));
+                }
+            }
+            string new_filename = Path.GetFileNameWithoutExtension(filename) + "_inverse" + Path.GetExtension(filename);
+            bitmap.Save(new_filename);
+        }
+    }
 
- for (int i = 0; i < bmap.Width; i++)
- {
- for (int j = 0; j < bmap.Height; j++)
- {
- c = bmap.GetPixel(i, j);
- byte gray = (byte)(.299 * c.R + .587 * c.G + .114 * c.B);
- bmap.SetPixel(i, j, Color.FromArgb(gray, gray, gray));
- }
- }
- temp = (Bitmap)bmap.Clone();
- temp.Save("C:\\Users\\Valerie\\holbertonschool-csharp\\image_processor\\" + fileGrayscale);
- }
- }
+    /// <summary> Inverse method </summary>
+    public static void BlackWhite(string[] filenames, double threshold) {
+    foreach (string file in filenames)
+    {
+    string fileWithoutImages = file.Remove(0, 7);
+    string fileWithoutImagesAndJPG = fileWithoutImages.Remove(fileWithoutImages.Length - 4, 4);
+    string fileBW = fileWithoutImagesAndJPG + "_bw" + ".jpg";
+    // Console.WriteLine(fileBW);
 
- /// <summary> Inverse method </summary>
- public static void BlackWhite(string[] filenames, double threshold) {
- foreach (string file in filenames)
- {
- string fileWithoutImages = file.Remove(0, 7);
- string fileWithoutImagesAndJPG = fileWithoutImages.Remove(fileWithoutImages.Length - 4, 4);
- string fileBW = fileWithoutImagesAndJPG + "_bw" + ".jpg";
- // Console.WriteLine(fileBW);
+    Bitmap temp = new Bitmap(file);
+    Bitmap bmap = (Bitmap)temp.Clone();
+    Color c;
 
- Bitmap temp = new Bitmap(file);
- Bitmap bmap = (Bitmap)temp.Clone();
- Color c;
-
- for (int i = 0; i < bmap.Width; i++)
- {
- for (int j = 0; j < bmap.Height; j++)
- {
- c = bmap.GetPixel(i, j);
- double luminance = ((c.R * 0.3) + (c.G * 0.59) + (c.B * 0.11));
- if (luminance < threshold)
- {
- bmap.SetPixel(i, j, Color.FromArgb(0, 0, 0));
- }
- else
- {
- bmap.SetPixel(i, j, Color.FromArgb(255, 255, 255));
- }
- //byte gray = (byte)(.299 * c.R + .587 * c.G + .114 * c.B);
- //bmap.SetPixel(i, j, Color.FromArgb(gray, gray, gray));
- }
- }
- temp = (Bitmap)bmap.Clone();
- temp.Save("C:\\Users\\Valerie\\holbertonschool-csharp\\image_processor\\" + fileBW);
- }
- }
+    for (int i = 0; i < bmap.Width; i++)
+    {
+    for (int j = 0; j < bmap.Height; j++)
+    {
+    c = bmap.GetPixel(i, j);
+    double luminance = ((c.R * 0.3) + (c.G * 0.59) + (c.B * 0.11));
+    if (luminance < threshold)
+    {
+    bmap.SetPixel(i, j, Color.FromArgb(0, 0, 0));
+    }
+    else
+    {
+    bmap.SetPixel(i, j, Color.FromArgb(255, 255, 255));
+    }
+    //byte gray = (byte)(.299 * c.R + .587 * c.G + .114 * c.B);
+    //bmap.SetPixel(i, j, Color.FromArgb(gray, gray, gray));
+    }
+    }
+    temp = (Bitmap)bmap.Clone();
+    temp.Save("C:\\Users\\Valerie\\holbertonschool-csharp\\image_processor\\" + fileBW);
+    }
+    }
 
  /// <summary> Thumbnail method </summary>
  public static void Thumbnail(string[] filenames, int height)
