19e261340ac9abd86ece0b62c2cd330c7d3a53d3
valerienierenberg <valerie.nierenberg@gmail.com>
2021-10-25 07:02:59 -0500

    Add xml doc for method

--- 0x07-csharp-tdd/0-add/MyMath/MyMath.cs
+++ 0x07-csharp-tdd/0-add/MyMath/MyMath.cs
@@ -5,6 +5,7 @@ namespace MyMath
     ///<summary>Operations class</summary>
     public class Operations
     {
+        ///<summary>Add method</summary>
         public static int Add(int a, int b)
         {
             return (a + b);
