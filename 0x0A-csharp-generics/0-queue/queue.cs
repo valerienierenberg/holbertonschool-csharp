86b21cf3bc0f8da5f4ad7f0f46c85ed74cc0f888
valerienierenberg <valerie.nierenberg@gmail.com>
2021-10-25 07:23:25 -0500

    Add task 0

---
+++ 0x0A-csharp-generics/0-queue/queue.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+
+///<summary>Queue Class</summary>
+class Queue<T>
+{
+    ///<summary>Check Type Method</summary>
+    public Type CheckType()
+    {
+        return (typeof(T));
+    }
+}
