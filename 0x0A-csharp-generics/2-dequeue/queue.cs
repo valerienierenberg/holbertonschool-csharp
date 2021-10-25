48062c691876b2aacd636648d123a993f9708c93
valerienierenberg <valerie.nierenberg@gmail.com>
2021-10-25 12:28:37 -0500

    Add task 2

---
+++ 0x0A-csharp-generics/2-dequeue/queue.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+
+///<summary>Queue Class</summary>
+class Queue<T>
+{
+    public Node head = null;
+    public Node tail = null;
+    public int count;
+
+    ///<summary>Check Type Method</summary>
+    public Type CheckType()
+    {
+        return (typeof(T));
+    }
+
+    ///<summary>Node class</summary>
+    public class Node
+    {
+        ///<summary>Node Value</summary>
+        public T value = default(T);
+        ///<summary>Next Node</summary>
+        public Node next = null;
+
+        public Node(T n)
+        {
+            value = n;
+        }
+    }
+
+    ///<summary>Enqueue Method</summary>
+    public void Enqueue(T value)
+    {
+        Node newNode = new Node(value);
+
+        if (head == null)
+        {
+            head = newNode;
+            //tail = newNode;
+        }
+        else if (tail == null)
+        {
+            tail = newNode;
+        }
+        else
+        {
+            tail.next = newNode;
+            tail = newNode;
+        }
+        count++;
+    }
+    public int Count()
+    {
+        return count;
+    }
+
+    public T Dequeue()
+    {
+        if (head == null)
+        {
+            Console.WriteLine("Queue is empty");
+            return default(T);
+        }
+        else
+        {
+            T value = head.value;
+            head = head.next;
+            count--;
+            return value;
+        }
+    }
+}
+
