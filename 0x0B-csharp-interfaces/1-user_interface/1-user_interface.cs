05d69499dc36c2899f3fc4e617228e4030d332f1
valerienierenberg <valerie.nierenberg@gmail.com>
2021-10-26 13:10:02 -0500

    Add XML Comments

--- 0x0B-csharp-interfaces/1-user_interface/1-user_interface.cs
+++ 0x0B-csharp-interfaces/1-user_interface/1-user_interface.cs
@@ -13,36 +13,51 @@ public abstract class Base
     }
 }
 
+///<summary>IInteractive interface</summary>
 public interface IInteractive
 {
+    ///<summary>Interact method</summary>
     void Interact();
 }
 
+///<summary>IBreakable interface</summary>
 public interface IBreakable
 {
+    ///<summary>durability setter</summary>
     int durability { get; set; }
+
+    ///<summary>Break method</summary>
     void Break();
 }
 
+///<summary>ICollectable interface</summary>
 public interface ICollectable
 {
+    ///<summary>isCollected setter</summary>
     bool isCollected { get; set; }
+
+    ///<summary>Break method</summary>
     void Collect();
 }
 
+///<summary>TestObject class</summary>
 public class TestObject : Base, IInteractive, IBreakable, ICollectable
 {
+    ///<summary>durability setter</summary>
     public int durability { get; set; } = 1;
+
+    ///<summary>isCollected setter</summary>
     public bool isCollected { get; set; } = false;
 
+    ///<summary>Interact method</summary>
     public void Interact()
     {
     }
-
+    ///<summary>Break method</summary>
     public void Break()
     {
     }
-
+    ///<summary>Collect method</summary>
     public void Collect()
     {
     }
