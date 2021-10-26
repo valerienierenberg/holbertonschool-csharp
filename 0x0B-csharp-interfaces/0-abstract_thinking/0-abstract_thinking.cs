eff922c659d8486d5a8a59e64af18ebd8155167c
valerienierenberg <valerie.nierenberg@gmail.com>
2021-10-26 07:26:10 -0500

    Add task 0

---
+++ 0x0B-csharp-interfaces/0-abstract_thinking/0-abstract_thinking.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+///<summary>Abstract Base class</summary>
+public abstract class Base
+{
+    ///<summary>Public property name</summary>
+    public string name = "";
+
+    ///<summary>ToString() override method</summary>
+    public override string ToString()
+    {
+        return $"{this.name}" + " is a " + $"{this.GetType()}";
+    }
+}
