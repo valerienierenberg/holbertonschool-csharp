cf2ce49e448f1bc978ce70d5cc7e495af9571346
valerienierenberg <valerie.nierenberg@gmail.com>
2021-10-26 07:34:40 -0500

    Add task 1

---
+++ 0x0B-csharp-interfaces/1-user_interface/1-user_interface.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+///<summary>Abstract Base class</summary>
+public abstract class Base
+{
+    ///<summary>Public property name</summary>
+    public string name { get; set; }
+
+    ///<summary>ToString() override method</summary>
+    public override string ToString()
+    {
+        return (this.name) + " is a " + (this.GetType());
+    }
+}
+
+public interface IInteractive
+{
+    void Interact();
+}
+
+public interface IBreakable
+{
+    int durability { get; set; }
+    void Break();
+}
+
+public interface ICollectable
+{
+    bool isCollected { get; set; }
+    void Collect();
+}
+
+public class TestObject : Base, IInteractive, IBreakable, ICollectable
+{
+    public int durability { get; set; } = 1;
+    public bool isCollected { get; set; } = false;
+
+    public void Interact()
+    {
+    }
+
+    public void Break()
+    {
+    }
+
+    public void Collect()
+    {
+    }
+}
