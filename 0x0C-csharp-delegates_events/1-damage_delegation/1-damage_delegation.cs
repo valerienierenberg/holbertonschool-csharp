951e14df1a7cc40e60c8fdea236da292b8ba658f
valerienierenberg <valerie.nierenberg@gmail.com>
2021-10-27 19:04:39 -0500

    Add task 1

--- 0x0C-csharp-delegates_events/1-damage_delegation/1-damage_delegation.cs
+++ 0x0C-csharp-delegates_events/1-damage_delegation/1-damage_delegation.cs
@@ -1,12 +1,64 @@
 ﻿using System;
 
-namespace _1_damage_delegation
+///<summary> Player class </summary>
+class Player
 {
-    class Program
+    private string name { get; set; }
+    private float maxHp { get; set; }
+    private float hp { get; set; }
+
+    ///<summary> Player constructor </summary>
+    public Player(string name = "Player", float maxHp = 100f)
     {
-        static void Main(string[] args)
+        if (maxHp < 0)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("maxHp must be greater than 0. maxHp set to 100f by default.");
+            this.maxHp = 100f;
+        }
+        else
+        {
+            this.maxHp = maxHp;
+        }
+        this.name = name;
+        this.hp = this.maxHp;
+    }
+
+    ///<summary> Prints the player's health status </summary>
+    public void PrintHealth()
+    {
+        Console.WriteLine("{0} has {1} / {2} health", this.name, this.hp, this.maxHp);
+    }
+
+
+    /// <summary> delegate CalculateHealth </summary>
+    delegate void CalculateHealth(float health);
+
+    /// <summary> HealDamage method </summary>
+    public void HealDamage(float heal)
+    {
+        if (heal < 0f)
+        {
+            heal = 0f;
+            Console.WriteLine($"{this.name} heals 0 HP!");
+        }
+        else
+        {
+            Console.WriteLine($"{this.name} heals {heal} HP!");
+        }
+    }
+
+    /// <summary> TakeDamage method </summary>
+    public void TakeDamage(float damage)
+    {
+        if (damage < 0f)
+        {
+            damage = 0f;
+            Console.WriteLine($"{this.name} takes 0 damage!");
+        }
+        else
+        {
+            Console.WriteLine($"{this.name} takes {damage} damage!");
         }
     }
 }
+
