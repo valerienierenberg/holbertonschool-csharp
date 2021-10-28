e9f93077f7dae6a6c1447660782d902e89e5cbe4
valerienierenberg <valerie.nierenberg@gmail.com>
2021-10-27 19:00:49 -0500

    add task 0

--- 0x0C-csharp-delegates_events/0-universal_health/0-universal_health.cs
+++ 0x0C-csharp-delegates_events/0-universal_health/0-universal_health.cs
@@ -1,12 +1,31 @@
 ﻿using System;
 
-namespace _0_universal_health
+///<summary> Player class </summary>
+class Player
 {
-    class Program
+    private string name { get; set; }
+    private float maxHp { get; set; }
+    private float hp { get; set; }
+
+    ///<summary> Player constructor </summary>
+    public Player(string name = "Player", float maxHp = 100f)
     {
-        static void Main(string[] args)
+        if (maxHp < 0)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("maxHp must be greater than 0. maxHp set to 100f by default.");
+            this.maxHp = 100f;
         }
+        else
+        {
+            this.maxHp = maxHp;
+        }
+        this.name = name;
+        this.hp = this.maxHp;
+    }
+
+    ///<summary> Prints the player's health status </summary>
+    public void PrintHealth()
+    {
+        Console.WriteLine("{0} has {1} / {2} health", this.name, this.hp, this.maxHp);
     }
 }
